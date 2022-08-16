7edbd43264cb6d725073b4d7460f89f36da46541
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-16 11:04:58 +0800

    Delete Quiz2-2016 directory

--- Quiz2-2016/1.Functor_(Tree_as_its_instance).hs
+++
@@ -1,9 +0,0 @@
-data Tree a b = Node a (Tree a b) (Tree a b) | Leaf b deriving (Show)
-
-tree1=  Node 'a' (Node 'b' (Leaf 4) (Leaf 6)) (Leaf 7)  
-
-instance Functor (Tree a) where
-        fmap f (Leaf b) = Leaf (f b)
-        fmap f (Node a lt rt) = (Node a (fmap f lt) (fmap f rt)) 
-
-sqTree= fmap (\x->x*x) tree1
