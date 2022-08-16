f5fd282eafcc07501158350d6548c8ba41432ac7
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-16 11:05:17 +0800

    Delete data directory

--- data/person.hs
+++
@@ -1,6 +0,0 @@
-data Person = Person {age::Int,name::String} deriving (Show)
-
-data StrPerson = StrPerson {sage::Int,sname::String}	
-
-instance Show StrPerson where
-           show StrPerson {sage=a,sname=n} = n ++ " has age " ++ show a ++ "." 
